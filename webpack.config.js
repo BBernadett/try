982f5f9359e6e2b43ce31bd80adb1c55944c2bc5
BBernadett <dettus.bb@gmail.com>
2023-05-31 22:50:53 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path'); 
+
+const CopyWebpackPlugin = require('copy-webpack-plugin'); 
+
+const ghpages = require('gh-pages');
+
+ghpages.publish('dist', function(err) {});
+
+const DIST_DIR = path.resolve(__dirname, 'dist'); 
+
+  
+
+module.exports = { 
+
+    entry: './app.js', 
+
+    output: { 
+
+        path: DIST_DIR, 
+
+        filename: 'bundle.js' 
+
+    }, 
+
+    plugins: [ 
+
+        new CopyWebpackPlugin({ 
+
+            patterns: [ 
+
+                { from: './index.html', to: DIST_DIR } 
+
+            ] 
+
+        }) 
+
+    ], 
+
+    module: { 
+
+        rules: [ 
+
+            { 
+
+                test: /\.css$/i, 
+
+                use: [ 
+
+                    "style-loader", 
+
+                    "css-loader" 
+
+                ] 
+
+            }, 
+
+        ] 
+
+    }, 
+
+    devServer: { 
+
+        static: { 
+
+            directory: DIST_DIR 
+
+        }, 
+
+    }, 
+
+    mode: 'development', 
+
+} 
